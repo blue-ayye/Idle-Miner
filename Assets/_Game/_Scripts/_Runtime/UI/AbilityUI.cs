3d61b26ee240cc72fe1b2bf9af0fd011d7e67d1a
blue-ayye <ccoolbuddy4@gmail.com>
2023-03-17 00:32:40 +0530

    Setup Abilities UI

---
+++ Assets/_Game/_Scripts/_Runtime/UI/AbilityUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityUI : MonoBehaviour
+{
+    [SerializeField] private Image _displayImage;
+    [SerializeField] private TMP_Text _titleText;
+    [SerializeField] private TMP_Text _levelText;
+    [SerializeField] private TMP_Text _descriptionText;
+    [SerializeField] private TMP_Text _costText;
+
+
+
+}
